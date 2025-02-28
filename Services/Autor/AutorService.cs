b734ddab9cc2ccbb5ff514ebf8835e39cacbcb11
Luan Bossardi <xdbilu@gmail.com>
2025-02-28 20:36:57 -0300

    Projeto Api criado

---
+++ Services/Autor/AutorService.cs
@@ -0,0 +1,28 @@
+﻿using WebApi8.Data;
+using WebApi8.Models;
+
+namespace WebApi8.Services.Autor
+{
+    public class AutorService : IAutorInterface
+    {
+        private readonly AppDbContext _context;
+        public AutorService(AppDbContext context)
+        {
+            _context = context;
+        }
+        public Task<ResponseModel<AutorModel>> BuscarAutorPorId(int idAutor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<AutorModel>> BuscarAutorPorIdLivro(int idLivro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<List<AutorModel>>> ListarAutores()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
